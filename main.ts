ac50180e11f8d14cdd9dcb16009c12b516a3b70a
deel34 <37339295+deel34@users.noreply.github.com>
2020-07-24 15:43:07 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Double))
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
+basic.showString("LET'S USE MORSE CODE")
+basic.pause(100)
+basic.clearScreen()
